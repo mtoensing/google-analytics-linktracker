c764822c2273640b823469cb126fe4c994c09051
Marc Tönsing <marc.toensing@gmail.com>
2017-12-06 20:31:23 +0100

    fixed naming

--- jquery.linktracker.js
+++ jquery.linktracker.js
@@ -13,10 +13,10 @@
             trackLink("a.cld-like-dislike-trigger", 'likedislike');
             trackLink("a.socialbutton", 'socialbutton');
             trackLink(".tags-links a", 'tags');
-            trackLink(".older-posts a", 'posts_older');
-            trackLink(".next.page-numbers", 'posts_next');
-            trackLink(".next.page-numbers", 'posts_next');
-            trackLink(".lastcommented a", 'last_commented');
+            trackLink(".older-posts a", 'posts-older');
+            trackLink(".next.page-numbers", 'posts-next');
+            trackLink(".prev.page-numbers", 'posts-prev');
+            trackLink(".lastcommented a", 'last-commented');
             trackLink(".gtc-link", 'most-popular');
         }
 
