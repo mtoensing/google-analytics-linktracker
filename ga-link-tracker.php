b3678b9d97cf06583e4ace10b782a65301be8518
Marc Tönsing <marc@marc.tv>
2020-09-04 15:39:37 +0200

    Update ga-link-tracker.php

--- ga-link-tracker.php
+++ ga-link-tracker.php
@@ -5,7 +5,7 @@
   Plugin URI: https://marc.tv
   GitHub Plugin URI: mtoensing/google-analytics-linktracker
   Description: Track internal links as events.
-  Version: 3.8
+  Version: 3.9
   Author: Marc Tönsing
   Author URI: https://marc.tv
   License: Creative Commons Attribution-ShareAlike
@@ -19,7 +19,7 @@
 
 function ga_link_tracker_load_scripts() {
 
-	$version = '3.8';
+	$version = '3.9';
 
 	wp_enqueue_script(
 		"linktracker-googleanalytics", WP_PLUGIN_URL . "/google-analytics-linktracker/jquery.linktracker.js", array( "jquery" ), $version, true );
